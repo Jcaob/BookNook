95946345d05893eac29c56728e900209d45d85e3
Kniswand <122379489+Kniswand@users.noreply.github.com>
2023-10-04 15:48:20 -0400

    Adding of module and the blog/cart pages

--- app/app.js
+++ app/app.js
@@ -1,3 +1,25 @@
 function toggleMenu(menu){
     menu.classList.toggle('open');
 }
+
+/* The following code is for the module */
+
+var pageName = "home"; // Define pageName globally
+
+function addPageContent(pageName) {
+    let pageContentName = pageName + "Content";
+    $(".app").html(window[pageContentName]); // Access the variable using window[pageContentName]
+}
+
+addPageContent("home");
+
+//addPageContent(pageName); // Pass the pageName as an argument
+
+function route() {
+    hashTag = window.location.hash;
+    console.log(hashTag);
+}
+
+$(window).on('hashchange', route);
+
+/* End of code for the module */
